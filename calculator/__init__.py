8dfa176e0b21061a5a6c5160ef228c02f912ef91
SamuelSudeepAyyala <sa2826@njit.edu>
2024-09-26 14:40:52 -0400

    __init__ changes in calculator commit

---
+++ calculator/__init__.py
@@ -0,0 +1,23 @@
+from calculator.operations import add, subtract, divide, multiply
+from calculator.Calculation import Calculation
+
+class Calculator:
+    @staticmethod
+    def add(a, b):
+        calculation = Calculation(a ,b, add)
+        return calculation.get_result()
+    
+    @staticmethod
+    def subtract(a, b):
+        calculation = Calculation(a ,b, subtract)
+        return calculation.get_result()
+
+    @staticmethod
+    def multiply(a, b):
+        calculation = Calculation(a ,b, multiply)
+        return calculation.get_result()
+    
+    @staticmethod
+    def divide(a, b):
+        calculation = Calculation(a, b, divide)
+        return calculation.get_result()
