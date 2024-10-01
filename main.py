3c7b568cd0cdbcdcfa7e495907e70896f293c14a
SamuelSudeepAyyala <sa2826@njit.edu>
2024-10-01 01:28:03 -0400

    Faker changes

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ import sys
 from calculator import Calculator
 from decimal import Decimal, InvalidOperation
 
-def calculate_and_print(a, b, operation_name) :
-    mappings = {
-        'add' : Calculator.add,
-        'subtract' : Calculator.subtract,
-        'multiply' : Calculator.multiply,
-        'divide' : Calculator.divide
+def calculate_and_print(a, b, operation_name):
+    operation_mappings = {
+        'add': Calculator.add,
+        'subtract': Calculator.subtract,
+        'multiply': Calculator.multiply,
+        'divide': Calculator.divide
     }
 
-    try : 
-        a_decimal, b_decimal = map(Decimal, [a,b])
-        result = mappings.get(operation_name)
-        if result : 
-            print(f"The result of {a_decimal} {operation_name} {b} is equal to {result(a_decimal, b_decimal)}")
-        else :
+    try:
+        a_decimal, b_decimal = map(Decimal, [a, b])
+        result = operation_mappings.get(operation_name)
+        if result:
+            print(f"The result of {a} {operation_name} {b} is equal to {result(a_decimal, b_decimal)}")
+        else:
             print(f"Unknown operation: {operation_name}")
-    except InvalidOperation :
+    except InvalidOperation:
         print(f"Invalid number input: {a} or {b} is not a valid number.")
     except ZeroDivisionError:
         print("Error: Division by zero.")
-    except Exception as e: 
+    except Exception as e:
         print(f"An error occurred: {e}")
 
 def main():
@@ -33,4 +33,4 @@ def main():
     calculate_and_print(a, b, operation)
 
 if __name__ == '__main__':
-        main()
+    main()
